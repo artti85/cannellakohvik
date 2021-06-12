7c69266e90fe715b80f67dd74f39901b6fe6f80d
artti85 <60928313+artti85@users.noreply.github.com>
2021-06-12 23:44:03 +0300

    1

---
+++ kohvik/script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // autoHeight: true, 
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    slidesPerView: 1, 
+    spaceBetween: 10,
+    // freeMode: true,
+
+    autoplay: {
+        delay: 2000,
+        // stopOnLastSlide: true,
+        disableOnInteraction: true,
+    },
+    speed: 900,
+    effect: 'fade',
+  });
