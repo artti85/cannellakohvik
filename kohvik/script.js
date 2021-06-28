6ee903efdf88934d18bda4d8a50dfc02e1f26727
artti85 <60928313+artti85@users.noreply.github.com>
2021-06-28 22:07:47 +0300

    12

--- kohvik/script.js
+++ kohvik/script.js
@@ -62,3 +62,19 @@ for (anchor of anchors) {
         })
     }
 }
+
+
+
+
+/* меню бургер */
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu');
+
+if(iconMenu){
+    iconMenu.addEventListener("click",function(e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+  
+}
